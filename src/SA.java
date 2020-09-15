77a3cf7687ec6f62b85d84312d3afcf2b55ce541
98christianp <christianpetersen98@gmail.com>
2020-09-15 15:30:04 +0200

    added solutionMap class within solution

--- src/SA.java
+++ src/SA.java
@@ -1,25 +1,24 @@
-import java.util.ArrayList;
-
 public class SA implements MetaHeuristic{
 
     private float alpha;
     private float t_start;
     private Solution solution;
-
     // TODO: I suggest it just being a float modelling run time or calls
     private float stop_Criteria;
 
-    //TODO: return array<Solution>
-    // Function should return a list of new solutions based on this.solution.
+    public SA(float alpha, float t_start, float stop_criteria, Solution solution1) {
+        this.alpha = alpha;
+        this.t_start = t_start;
+        stop_Criteria = stop_criteria;
+        this.solution = solution1;
+    }
+
+    /**
+     * TODO: Generates the neighbourhood - method? Random choice? 2-opt
+     */
     public void generateNeighbourhood() {
         System.out.println("not implemented");
     }
-
-
-
-
-
-
 }
 
 
--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,41 @@
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Solution {
-    public ArrayList<Task> tasks;
-    public float score;
+    public ArrayList<SolutionMap> tasks = new ArrayList<>();
+    public float cost;
+    public float mcp;
 
-    public void addToArray(Task t) {
+    /**
+     * Add
+     * @param t (task to add)
+     */
+    public void addToArray(SolutionMap t) {
         this.tasks.add(t);
     }
+
+    /**
+     * TODO: Calculates cost given mapped solution
+     */
+    private void calculateCost() {
+        this.cost = 0;
+    }
+
+}
+
+class SolutionMap {
+    public float Id;
+    public float MCP;
+    public float Core;
+    public float WRCT;
+
+    public SolutionMap(float id, float MCP, float core, float WRCT) {
+        this.Id = id;
+        this.MCP = MCP;
+        this.Core = core;
+        this.WRCT = WRCT;
+    }
+
+
+
 }
