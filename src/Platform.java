93831a85addcf0df08ce3ac51e42185107497a26
Szabolcs <szabolcs@propairflight.com>
2020-09-15 15:41:38 +0200

    Core, Platform

--- src/Platform.java
+++ src/Platform.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Platform {
+    final ArrayList<Core> cores;
+
+    public Platform(ArrayList<Core> Cores)
+    {
+        cores = Cores;
+    }
+    public void AssignTasks()
+    {
+
+    }
 }
