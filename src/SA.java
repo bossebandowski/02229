755df012065c2b6da871ce57872aca1971fc8357
Christian-Petersen1 <Christian-Petersen1@ibm.com>
2020-09-15 14:30:38 +0200

    placeholder for solution class and SA class

--- src/SA.java
+++ src/SA.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class SA implements MetaHeuristic{
+
+    private float alpha;
+    private float t_start;
+    private Solution solution;
+
+    // TODO: I suggest it just being a float modelling run time or calls
+    private float stop_Criteria;
+
+    //TODO: return array<Solution>
+    // Function should return a list of new solutions based on this.solution.
+    public void generateNeighbourhood() {
+        System.out.println("not implemented");
+    }
+
+
+
+
+
+
 }
+
+
---
+++ src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Solution {
+    public ArrayList<Task> tasks;
+    public float score;
+
+    public void addToArray(Task t) {
+        this.tasks.add(t);
+    }
+}
