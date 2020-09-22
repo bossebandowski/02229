c4d1b8764eec3f221147015892753aa66a7aaece
bossebandowski <36918764+bossebandowski@users.noreply.github.com>
2020-09-22 15:55:45 +0200

    pre core fix

--- src/Platform.java
+++ src/Platform.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Platform {
-
-
     final ArrayList<Core> cores;
 
     public Platform(ArrayList<Core> Cores)
@@ -10,22 +9,16 @@ public class Platform {
         cores = Cores;
     }
 
-    public ArrayList<Core> getCores() {
-        return cores;
-    }
-
-    public Core get_core(String coreId){
-        for (Core core : cores){
-            if (core.getId() == coreId){
-                return core;
+    public void AssignTasks(String coreID, ArrayList<Task> tasks)
+    {
+        Iterator<Core> coreIterator = cores.iterator();
+        while(coreIterator.hasNext())
+        {
+            Core currentCore = coreIterator.next();
+            if(currentCore.getID().equals(coreID))
+            {
+                currentCore.addTask(tasks);
             }
         }
-        return null;
     }
-
-    public void AssignTasks()
-    {
-
-    }
-
 }
