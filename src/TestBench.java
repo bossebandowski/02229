0e67e1a40dde72f17a043688c4f6da61d880f944
bossebandowski <36918764+bossebandowski@users.noreply.github.com>
2020-09-22 13:55:09 +0200

    refactor of folder structure before TestBench implementation

---
+++ src/TestBench.java
@@ -0,0 +1,2 @@
+public class TestBench {
+}
--- test/TestBench.java
+++
@@ -1,8 +0,0 @@
-// create package of source code, import here
-
-public class TestBench {
-    public static void main(String[] args) {
-        // run Scheduler.main() with all test file paths
-        String outPath = "../testResults/";
-    }
-}
